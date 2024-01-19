9fb7dc2c2167819c9a4c5639efc233938258a0be
Enda <endamagennis@gmail.com>
2024-01-19 17:37:14 +0000

    Completed homepage header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '.codeanyapp.com',
     '.herokuapp.com',
+    '127.0.0.1',
 ]
 
 
@@ -150,6 +151,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
