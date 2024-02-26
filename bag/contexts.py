8da6a601e7d9bf823a89399c5c065ef433689247
Enda <endamagennis@gmail.com>
2024-02-26 15:02:38 +0000

    Added toasts

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,7 +12,6 @@ def bag_contents(request):
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
-
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
             product_count += item_data
@@ -24,7 +23,6 @@ def bag_contents(request):
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                
                 total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
@@ -36,19 +34,19 @@ def bag_contents(request):
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_detla = settings.FREE_DELIVERY_THRESHOLD - total
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
-        free_delivery_detla = 0
-
+        free_delivery_delta = 0
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
-        'free_delivery_detla': free_delivery_detla,
+        'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
--- products/views.py
+++ products/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q 
-from .models import Product, Category
+from django.db.models import Q
 from django.db.models.functions import Lower
 
+from .models import Product, Category
 
 # Create your views here.
+
 def all_products(request):
-    ''' A view to return all products, including sorting and search queries'''
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -21,46 +22,43 @@ def all_products(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))  # this is to sort by name in alphabetical order
+                products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)  # this is to sort by price in ascending order
+            products = products.order_by(sortkey)
             
-
-        if 'category' in request.GET:  # if category is in the request.GET dictionary
+        if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)  # filter the products by the categories
-            categories = Category.objects.filter(name__in=categories)  # filter the categories by the categories
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-        
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(description__icontains=query)  # Q is a complex query
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'{sort}_{direction}'  # this is for the sorting to keep the sorting in the search bar
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
-        'search_term': query,  # this is for the search bar to keep the search term in the search bar
-        'current_categories': categories,  # this is for the category filter to keep the category in the search bar
-        'current_sorting': current_sorting,  # this is for the sorting to keep the sorting in the search bar
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-    ''' A view to show individual product details '''
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
